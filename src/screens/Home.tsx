c8c8ab94717228325c101bb4b376fc48a6ba86b1
azhararrozak <azhardrozak2001@upi.edu>
2024-06-27 07:19:22 +0700

    Add nested navigation in home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 
-export default function Home(): JSX.Element {
+export default function Home({ navigation }: { navigation:any }): JSX.Element {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text>Movie Page</Text>
+      <Button
+        title="Pergi ke Movie Detail"
+        onPress={() => {
+          navigation.navigate('MovieDetail')
+        }}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marginVertical20: {
+    marginVertical: 20,
+  },
+})
---
+++ src/screens/MovieDetail.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MovieDetail(): JSX.Element {
+  return (
+    <View>
+      <Text>MovieDetail</Text>
+    </View>
+  )
+}
