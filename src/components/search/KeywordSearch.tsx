7b5fce820b7cdda44c7fd57c4e90336e1d7e22ea
azhararrozak <azhardrozak2001@upi.edu>
2024-06-28 22:25:14 +0700

    Add icon search in input text

--- src/components/search/KeywordSearch.tsx
+++ src/components/search/KeywordSearch.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
 import MovieItem from '../movies/MovieItem';
 import { FontAwesome } from '@expo/vector-icons'
 
-const API_KEY = process.env.EXPO_PUBLIC_API_ACCESS_TOKEN; // Ganti dengan kunci API TMDB Anda
+const API_KEY = process.env.EXPO_PUBLIC_API_ACCESS_TOKEN;
 
 export default function KeywordSearch(): JSX.Element {
   const [keyword, setKeyword] = useState<string>('');
@@ -15,7 +15,7 @@ export default function KeywordSearch(): JSX.Element {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${API_KEY}`, // Pastikan API_ACCESS_TOKEN didefinisikan sesuai kebutuhan Anda
+        Authorization: `Bearer ${API_KEY}`,
       },
     };
     try {
@@ -32,12 +32,17 @@ export default function KeywordSearch(): JSX.Element {
 
   return (
     <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setKeyword(text)}
-        value={keyword}
-        onSubmitEditing={handleSearch}
-      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setKeyword(text)}
+          value={keyword}
+          placeholder="Search for a keyword"
+          placeholderTextColor="white"
+          onSubmitEditing={handleSearch}
+        />
+        <FontAwesome name="search" size={20} color="white" style={styles.searchIcon} />
+      </View>
       <FlatList
         data={searchResults}
         renderItem={({ item }) => (
@@ -55,11 +60,26 @@ export default function KeywordSearch(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
+  
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#382645',
+    borderRadius: 20,
+    marginVertical: 20,
+    marginHorizontal: 10,
+    paddingHorizontal: 15,
+  },
+  searchIcon: {
+    position: 'absolute',
+    left: 320,
+    zIndex: 1,
+  },
   input: {
     height: 50,
     paddingHorizontal: 15,
     borderRadius: 20,
-    marginVertical: 20,
+    flex: 1,
     color: 'white',
     backgroundColor: '#382645'
   },
